4af943ed4bdc70577674a44f497131f0f6cbfbb9
Margarita Vicuna <margaritamvicuna@gmail.com>
2025-03-01 19:38:06 -0300

    ✨ add basic pages

---
+++ app/pages/guessing.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Guessing Game</h1>
+  </div>
+</template>
+
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <button @click="navigateTo('/guessing')">Guessing Game</button>
+  </div>
+</template>
+
+
+
