d52eeb49a6752615059495e8e9209890b05251a0
Margarita Vicuna <margaritamvicuna@gmail.com>
2025-03-01 19:48:20 -0300

    ➕ add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+
   future: {
     compatibilityVersion: 4,
   },
+
   devtools: { enabled: true },
+
   experimental: {
     scanPageMeta: "after-resolve",
     sharedPrerenderData: false,
@@ -20,9 +23,12 @@ export default defineNuxtConfig({
       },
     },
   },
-   unhead: {
-    renderSSRHeadOptions: {
-      omitLineBreaks: false,
-    },
-  },
+
+  unhead: {
+   renderSSRHeadOptions: {
+     omitLineBreaks: false,
+   },
+ },
+
+  modules: ["@nuxtjs/tailwindcss"],
 });
