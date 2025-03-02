0cdf4d29be2888b63cdef8230ead59ba484db640
Margarita Vicuna <margaritamvicuna@gmail.com>
2025-03-01 21:54:51 -0300

    💄 config basic styles and fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: "Poppins, sans-serif",
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
