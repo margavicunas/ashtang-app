e9c54afb368f4e430c587e2904cbdbd06f2a3d8e
Margarita Vicuna <margaritamvicuna@gmail.com>
2025-09-19 19:17:45 -0300

    🚨 fix linter issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
 
   future: {
     compatibilityVersion: 4,
   },
 
   devtools: { enabled: true },
-
   experimental: {
-    scanPageMeta: "after-resolve",
+    scanPageMeta: 'after-resolve',
     sharedPrerenderData: false,
     compileTemplate: true,
     resetAsyncDataToUndefined: true,
     templateUtils: true,
     relativeWatchPaths: true,
     normalizeComponentNames: false,
-    spaLoadingTemplateLocation: "within",
+    spaLoadingTemplateLocation: 'within',
     defaults: {
       useAsyncData: {
         deep: true,
       },
     },
   },
-
   unhead: {
-   renderSSRHeadOptions: {
-     omitLineBreaks: false,
-   },
- },
-
-  modules: ["@nuxtjs/google-fonts", "@pinia/nuxt", "@nuxt/ui", "@nuxt/image"],
-});
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+  image: {
+    domains: ['www.devvratyoga.com'],
+  },
+  googleFonts: {
+    families: {
+      Poppins: true,
+    },
+  },
+  colorMode: {
+    preference: 'light',
+  },
+  modules: ['@nuxtjs/google-fonts', '@pinia/nuxt', '@nuxt/ui', '@nuxt/image'],
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
-  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
     extend: {
       fontFamily: {
-        Poppins: "Poppins, sans-serif",
+        Poppins: 'Poppins, sans-serif',
       },
       container: {
         center: true,
-        padding: "2rem",
+        padding: '2rem',
       },
     },
   },
   plugins: [],
-};
+}
