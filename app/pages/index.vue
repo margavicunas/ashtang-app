27392e09251c713be80886ad2f12b2b8595af24a
Margarita Vicuna <margaritamvicuna@gmail.com>
2025-09-19 19:23:25 -0300

    ✨ implement game store in home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <button @click="navigateTo('/guessing')">Guessing Game</button>
+  <div class="flex flex-col items-center justify-center h-screen gap-5">
+    <h1 class="text-2xl font-bold text-center">Ashtanga Primary Series Postures</h1>
+    <UButton @click="startGameHandler">Start</UButton>
   </div>
 </template>
 
+<script setup lang="ts">
+import { useGameStore } from '~/stores/game'
 
+const gameStore = useGameStore()
+const { startGame } = gameStore
 
+const startGameHandler = () => {
+  startGame()
+  navigateTo('/guessing-rounds')
+}
+</script>
