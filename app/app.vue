23039480a3627541f73ffc96b4daaf2425feaf91
Margarita Vicuna <margaritamvicuna@gmail.com>
2025-09-19 19:18:16 -0300

    🚧 add default layout for pages

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-100">
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="font-Poppins flex items-center justify-center h-screen">
+    <div class="md:w-3/4 w-full">
+      <slot />
+    </div>
+  </div>
+</template>
