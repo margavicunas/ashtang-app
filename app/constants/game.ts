04258eb6fce425f0708bb42b4efeb8ab98c64768
Margarita Vicuna <margaritamvicuna@gmail.com>
2025-09-20 22:24:13 -0300

    ✨ set `MAX_ROUNDS` to 10

--- app/constants/game.ts
+++ app/constants/game.ts
@@ -1,4 +1,4 @@
-export const MAX_ROUNDS = 4
+export const MAX_ROUNDS = 10
 export const TOTAL_WRONG_OPTIONS_COUNT = 3
 // TODO: Idea, make it configurable by choosing levels of difficulty
 export const SIMILAR_OPTIONS_COUNT = 2
